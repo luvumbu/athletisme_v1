d13509aeb7d243e02e31ee61311f7833ea5bc98f
luvumbu <luvumbu.n@gmail.com>
2022-04-03 05:56:53 +0200

    nouvelle methode creation fichier de resultat

--- model/class/php/get_cookie_creation_info_header.php
+++ model/class/php/get_cookie_creation_info_header.php
@@ -5,6 +5,10 @@ $type_search_variable_initial =$_POST["type_search_variable_initial"] ;
 $value =$_POST["value"] ;
 //$_SESSION['value'] = $value;
 
+$_SESSION['type_search_variable_initial'] = $type_search_variable_initial;
+$_SESSION['value'] = $value;
+
+
 echo $type_search_variable_initial; 
 echo $value; 
 //echo $values; 
@@ -12,32 +16,5 @@ echo $value;
 
 
 
-<?php
-
-$dbname="u481158665_ffa";
-$username="u481158665_ffa";
-$password="v3p9r3e@59A";
-$servername="localhost";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = 'SELECT * FROM `info_all` WHERE `get_users_nom_complet`="DOVY Lueyi"';
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "<br>" . $row["get_users_nom_complet"];
-  }
-} else {
-  echo "0 results";
-}
-$conn->close();
-?>
 
 
